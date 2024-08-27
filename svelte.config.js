5d0bb9ec583782cab8e648cb5ce9da1d10f23762
max <max.ind@spectre.solutions>
2024-08-27 06:19:20 +0200

    Done I thinkg

--- svelte.config.js
+++ svelte.config.js
@@ -2,22 +2,25 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
-	adapter: adapter({
-		pages: 'build',
-		assets: 'build',
-		fallback: undefined,
-		precompress: false,
-		strict: true,
-	}),
-	paths: {
-		base: dev ? '' : '/portfolio',
-	},
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true,
+		}),
+		paths: {
+			base: dev ? '' : '/portfolio',
+		},
+	}
 };
 
 export default config;
